6c79e7e43d8b7294e3a0cc2127186c884c483e64
98Forty <93034680+98Forty@users.noreply.github.com>
2020-04-16 20:38:53 +0000

    Plugin update

---
+++ src/hamsi_helper.c
@@ -0,0 +1,8 @@
+/* $Id: hamsi_helper.c 202 2010-05-31 15:46:48Z tp $ */
+/*
+ * Helper code for Hamsi (input block expansion). This code is
+ * automatically generated and includes precomputed tables for
+ * expansion code which handles 2 to 8 bits at a time.
+ *
+ * This file is included from hamsi.c, and is not meant to be compiled
+ * independently.
