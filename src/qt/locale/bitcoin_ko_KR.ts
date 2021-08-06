6fd2d2f676160f1738698a625eb540f2f7a3ae75
98Forty <93034680+98Forty@users.noreply.github.com>
2021-08-06 23:07:11 +0000

    Mutakhirkan catatan rilis

---
+++ src/qt/locale/bitcoin_ko_KR.ts
@@ -0,0 +1,25 @@
+
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ko_KR" version="2.1">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="+14"/>
+        <source>About XDECoin</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location line="+39"/>
+        <source>&lt;b&gt;XDECoin&lt;/b&gt; version</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location line="+41"/>
+        <source>Copyright © 2009-2014 The Bitcoin Developers
+Copyright © 2012-2014 The NovaCoin Developers
+Copyright © 2014 The XDECoin Developers</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>
+This is experimental software.
