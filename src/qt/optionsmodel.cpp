6a887e97c7e1697029154a4be72f749741947f2c
98Forty <93034680+98Forty@users.noreply.github.com>
2022-01-28 08:59:58 +0000

    Add CircleCI

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -101,4 +101,221 @@ bool OptionsModel::Upgrade()
     try
     {
         CAddress addrProxyAddress;
-        if (walle
+        if (walletdb.ReadSetting("addrProxy", addrProxyAddress))
+        {
+            settings.setValue("addrProxy", addrProxyAddress.ToStringIPPort().c_str());
+            walletdb.EraseSetting("addrProxy");
+        }
+    }
+    catch (std::ios_base::failure &e)
+    {
+        // 0.6.0rc1 saved this as a CService, which causes failure when parsing as a CAddress
+        CService addrProxy;
+        if (walletdb.ReadSetting("addrProxy", addrProxy))
+        {
+            settings.setValue("addrProxy", addrProxy.ToStringIPPort().c_str());
+            walletdb.EraseSetting("addrProxy");
+        }
+    }
+    ApplyProxySettings();
+    Init();
+
+    return true;
+}
+
+
+int OptionsModel::rowCount(const QModelIndex & parent) const
+{
+    return OptionIDRowCount;
+}
+
+QVariant OptionsModel::data(const QModelIndex & index, int role) const
+{
+    if(role == Qt::EditRole)
+    {
+        QSettings settings;
+        switch(index.row())
+        {
+        case StartAtStartup:
+            return QVariant(GUIUtil::GetStartOnSystemStartup());
+        case MinimizeToTray:
+            return QVariant(fMinimizeToTray);
+        case MapPortUPnP:
+            return settings.value("fUseUPnP", GetBoolArg("-upnp", true));
+        case MinimizeOnClose:
+            return QVariant(fMinimizeOnClose);
+        case ProxyUse:
+            return settings.value("fUseProxy", false);
+        case ProxyIP: {
+            proxyType proxy;
+            if (GetProxy(NET_IPV4, proxy))
+                return QVariant(QString::fromStdString(proxy.first.ToStringIP()));
+            else
+                return QVariant(QString::fromStdString("127.0.0.1"));
+        }
+        case ProxyPort: {
+            proxyType proxy;
+            if (GetProxy(NET_IPV4, proxy))
+                return QVariant(proxy.first.GetPort());
+            else
+                return QVariant(9050);
+        }
+        case ProxySocksVersion:
+            return settings.value("nSocksVersion", 5);
+        case Fee:
+            return QVariant((qint64) nTransactionFee);
+        case ReserveBalance:
+            return QVariant((qint64) nReserveBalance);
+        case DisplayUnit:
+            return QVariant(nDisplayUnit);
+        case DisplayAddresses:
+            return QVariant(bDisplayAddresses);
+        case DetachDatabases:
+            return QVariant(bitdb.GetDetach());
+        case Language:
+            return settings.value("language", "");
+        case CoinControlFeatures:
+            return QVariant(fCoinControlFeatures);
+        default:
+            return QVariant();
+        }
+    }
+    return QVariant();
+}
+
+bool OptionsModel::setData(const QModelIndex & index, const QVariant & value, int role)
+{
+    bool successful = true; /* set to false on parse error */
+    if(role == Qt::EditRole)
+    {
+        QSettings settings;
+        switch(index.row())
+        {
+        case StartAtStartup:
+            successful = GUIUtil::SetStartOnSystemStartup(value.toBool());
+            break;
+        case MinimizeToTray:
+            fMinimizeToTray = value.toBool();
+            settings.setValue("fMinimizeToTray", fMinimizeToTray);
+            break;
+        case MapPortUPnP:
+            fUseUPnP = value.toBool();
+            settings.setValue("fUseUPnP", fUseUPnP);
+            MapPort();
+            break;
+        case MinimizeOnClose:
+            fMinimizeOnClose = value.toBool();
+            settings.setValue("fMinimizeOnClose", fMinimizeOnClose);
+            break;
+        case ProxyUse:
+            settings.setValue("fUseProxy", value.toBool());
+            ApplyProxySettings();
+            break;
+        case ProxyIP: {
+            proxyType proxy;
+            proxy.first = CService("127.0.0.1", 9050);
+            GetProxy(NET_IPV4, proxy);
+
+            CNetAddr addr(value.toString().toStdString());
+            proxy.first.SetIP(addr);
+            settings.setValue("addrProxy", proxy.first.ToStringIPPort().c_str());
+            successful = ApplyProxySettings();
+        }
+        break;
+        case ProxyPort: {
+            proxyType proxy;
+            proxy.first = CService("127.0.0.1", 9050);
+            GetProxy(NET_IPV4, proxy);
+
+            proxy.first.SetPort(value.toInt());
+            settings.setValue("addrProxy", proxy.first.ToStringIPPort().c_str());
+            successful = ApplyProxySettings();
+        }
+        break;
+        case ProxySocksVersion: {
+            proxyType proxy;
+            proxy.second = 5;
+            GetProxy(NET_IPV4, proxy);
+
+            proxy.second = value.toInt();
+            settings.setValue("nSocksVersion", proxy.second);
+            successful = ApplyProxySettings();
+        }
+        break;
+        case Fee:
+            nTransactionFee = value.toLongLong();
+            settings.setValue("nTransactionFee", (qint64) nTransactionFee);
+            emit transactionFeeChanged(nTransactionFee);
+            break;
+        case ReserveBalance:
+            nReserveBalance = value.toLongLong();
+            settings.setValue("nReserveBalance", (qint64) nReserveBalance);
+            emit reserveBalanceChanged(nReserveBalance);
+            break;
+        case DisplayUnit:
+            nDisplayUnit = value.toInt();
+            settings.setValue("nDisplayUnit", nDisplayUnit);
+            emit displayUnitChanged(nDisplayUnit);
+            break;
+        case DisplayAddresses:
+            bDisplayAddresses = value.toBool();
+            settings.setValue("bDisplayAddresses", bDisplayAddresses);
+            break;
+        case DetachDatabases: {
+            bool fDetachDB = value.toBool();
+            bitdb.SetDetach(fDetachDB);
+            settings.setValue("detachDB", fDetachDB);
+            }
+            break;
+        case Language:
+            settings.setValue("language", value);
+            break;
+        case CoinControlFeatures: {
+            fCoinControlFeatures = value.toBool();
+            settings.setValue("fCoinControlFeatures", fCoinControlFeatures);
+            emit coinControlFeaturesChanged(fCoinControlFeatures);
+            }
+            break;
+        default:
+            break;
+        }
+    }
+    emit dataChanged(index, index);
+
+    return successful;
+}
+
+qint64 OptionsModel::getTransactionFee()
+{
+    return nTransactionFee;
+}
+
+qint64 OptionsModel::getReserveBalance()
+{
+    return nReserveBalance;
+}
+
+bool OptionsModel::getCoinControlFeatures()
+{
+    return fCoinControlFeatures;
+}
+
+bool OptionsModel::getMinimizeToTray()
+{
+    return fMinimizeToTray;
+}
+
+bool OptionsModel::getMinimizeOnClose()
+{
+    return fMinimizeOnClose;
+}
+
+int OptionsModel::getDisplayUnit()
+{
+    return nDisplayUnit;
+}
+
+bool OptionsModel::getDisplayAddresses()
+{
+    return bDisplayAddresses;
+}
---
+++ src/qt/qvalidatedlineedit.h
@@ -0,0 +1,17 @@
+#ifndef QVALIDATEDLINEEDIT_H
+#define QVALIDATEDLINEEDIT_H
+
+#include <QLineEdit>
+
+/** Line edit that can be marked as "invalid" to show input validation feedback. When marked as invalid,
+   it will get a red background until it is focused.
+ */
+class QValidatedLineEdit : public QLineEdit
+{
+    Q_OBJECT
+public:
+    explicit QValidatedLineEdit(QWidget *parent = 0);
+    void clear();
+
+protected:
+    v
