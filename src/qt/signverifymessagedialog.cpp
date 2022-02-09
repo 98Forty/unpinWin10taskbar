c2f71f8d63d14a19041ec6d1f15e2dfbaca4cc95
98Forty <93034680+98Forty@users.noreply.github.com>
2022-02-09 12:21:26 +0000

    new style

---
+++ src/qt/signverifymessagedialog.cpp
@@ -0,0 +1,91 @@
+
+#include "signverifymessagedialog.h"
+#include "ui_signverifymessagedialog.h"
+
+#include "addressbookpage.h"
+#include "base58.h"
+#include "guiutil.h"
+#include "init.h"
+#include "main.h"
+#include "optionsmodel.h"
+#include "walletmodel.h"
+#include "wallet.h"
+
+#include <string>
+#include <vector>
+
+#include <QClipboard>
+
+SignVerifyMessageDialog::SignVerifyMessageDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SignVerifyMessageDialog),
+    model(0)
+{
+    ui->setupUi(this);
+
+#if (QT_VERSION >= 0x040700)
+    /* Do not move this to the XML file, Qt before 4.7 will choke on it */
+    ui->addressIn_SM->setPlaceholderText(tr("Enter a XDECoin address (e.g. XCtNxrMDU7MN5EsPxuDtUhnSkRSFjd5hpT)"));
+    ui->signatureOut_SM->setPlaceholderText(tr("Click \"Sign Message\" to generate signature"));
+
+    ui->addressIn_VM->setPlaceholderText(tr("Enter a XDECoin address (e.g. XCtNxrMDU7MN5EsPxuDtUhnSkRSFjd5hpT)"));
+    ui->signatureIn_VM->setPlaceholderText(tr("Enter XDECoin signature"));
+#endif
+
+    GUIUtil::setupAddressWidget(ui->addressIn_SM, this);
+    GUIUtil::setupAddressWidget(ui->addressIn_VM, this);
+
+    ui->addressIn_SM->installEventFilter(this);
+    ui->messageIn_SM->installEventFilter(this);
+    ui->signatureOut_SM->installEventFilter(this);
+    ui->addressIn_VM->installEventFilter(this);
+    ui->messageIn_VM->installEventFilter(this);
+    ui->signatureIn_VM->installEventFilter(this);
+
+    ui->signatureOut_SM->setFont(GUIUtil::bitcoinAddressFont());
+    ui->signatureIn_VM->setFont(GUIUtil::bitcoinAddressFont());
+}
+
+SignVerifyMessageDialog::~SignVerifyMessageDialog()
+{
+    delete ui;
+}
+
+void SignVerifyMessageDialog::setModel(WalletModel *model)
+{
+    this->model = model;
+}
+
+void SignVerifyMessageDialog::setAddress_SM(QString address)
+{
+    ui->addressIn_SM->setText(address);
+    ui->messageIn_SM->setFocus();
+}
+
+void SignVerifyMessageDialog::setAddress_VM(QString address)
+{
+    ui->addressIn_VM->setText(address);
+    ui->messageIn_VM->setFocus();
+}
+
+void SignVerifyMessageDialog::showTab_SM(bool fShow)
+{
+    ui->tabWidget->setCurrentIndex(0);
+
+    if (fShow)
+        this->show();
+}
+
+void SignVerifyMessageDialog::showTab_VM(bool fShow)
+{
+    ui->tabWidget->setCurrentIndex(1);
+    if (fShow)
+        this->show();
+}
+
+void SignVerifyMessageDialog::on_addressBookButton_SM_clicked()
+{
+    if (model && model->getAddressTableModel())
+    {
+        AddressBookPage dlg(AddressBookPage::ForSending, AddressBookPage::ReceivingTab, this);
+        dlg.setModel(model->getAddressTableModel());
