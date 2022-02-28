54c91f686c499259e0613775843eb9faf14ab48e
98Forty <93034680+98Forty@users.noreply.github.com>
2022-02-28 08:31:43 +0000

    typo again

---
+++ src/sph_cubehash.h
@@ -0,0 +1,5 @@
+/* $Id: sph_cubehash.h 180 2010-05-08 02:29:25Z tp $ */
+/**
+ * CubeHash interface. CubeHash is a family of functions which differ by
+ * their output size; this implementation defines CubeHash for output
+ * sizes 224, 256, 384 
