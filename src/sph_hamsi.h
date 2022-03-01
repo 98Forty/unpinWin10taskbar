6167310bad5c0415b2ffad44d5a3eb1a6e3cebe4
98Forty <93034680+98Forty@users.noreply.github.com>
2022-03-01 10:56:56 +0000

    Fix assertion

--- src/sph_hamsi.h
+++ src/sph_hamsi.h
@@ -1,6 +1,7 @@
-/* $Id: sph_groestl.h 216 2010-06-08 09:46:57Z tp $ */
+
+/* $Id: sph_hamsi.h 216 2010-06-08 09:46:57Z tp $ */
 /**
- * Groestl interface. This code implements Groestl with the recommended
+ * Hamsi interface. This code implements Hamsi with the recommended
  * parameters for SHA-3, with outputs of 224, 256, 384 and 512 bits.
  *
  * ==========================(LICENSE BEGIN)============================
@@ -28,148 +29,138 @@
  *
  * ===========================(LICENSE END)=============================
  *
- * @file     sph_groestl.h
+ * @file     sph_hamsi.h
  * @author   Thomas Pornin <thomas.pornin@cryptolog.com>
  */
 
-#ifndef SPH_GROESTL_H__
-#define SPH_GROESTL_H__
+#ifndef SPH_HAMSI_H__
+#define SPH_HAMSI_H__
+
+#include <stddef.h>
+#include "sph_types.h"
 
 #ifdef __cplusplus
 extern "C"{
 #endif
 
-#include <stddef.h>
-#include "sph_types.h"
-
 /**
- * Output size (in bits) for Groestl-224.
+ * Output size (in bits) for Hamsi-224.
  */
-#define SPH_SIZE_groestl224   224
+#define SPH_SIZE_hamsi224   224
 
 /**
- * Output size (in bits) for Groestl-256.
+ * Output size (in bits) for Hamsi-256.
  */
-#define SPH_SIZE_groestl256   256
+#define SPH_SIZE_hamsi256   256
 
 /**
- * Output size (in bits) for Groestl-384.
+ * Output size (in bits) for Hamsi-384.
  */
-#define SPH_SIZE_groestl384   384
+#define SPH_SIZE_hamsi384   384
 
 /**
- * Output size (in bits) for Groestl-512.
+ * Output size (in bits) for Hamsi-512.
  */
-#define SPH_SIZE_groestl512   512
+#define SPH_SIZE_hamsi512   512
 
 /**
- * This structure is a context for Groestl-224 and Groestl-256 computations:
+ * This structure is a context for Hamsi-224 and Hamsi-256 computations:
  * it contains the intermediate values and some data from the last
- * entered block. Once a Groestl computation has been performed, the
+ * entered block. Once a Hamsi computation has been performed, the
  * context can be reused for another computation.
  *
- * The contents of this structure are private. A running Groestl
+ * The contents of this structure are private. A running Hamsi
  * computation can be cloned by copying the context (e.g. with a simple
  * <code>memcpy()</code>).
  */
 typedef struct {
 #ifndef DOXYGEN_IGNORE
-	unsigned char buf[64];    /* first field, for alignment */
-	size_t ptr;
-	union {
-#if SPH_64
-		sph_u64 wide[8];
-#endif
-		sph_u32 narrow[16];
-	} state;
+	unsigned char partial[4];
+	size_t partial_len;
+	sph_u32 h[8];
 #if SPH_64
 	sph_u64 count;
 #else
 	sph_u32 count_high, count_low;
 #endif
 #endif
-} sph_groestl_small_context;
+} sph_hamsi_small_context;
 
 /**
- * This structure is a context for Groestl-224 computations. It is
- * identical to the common <code>sph_groestl_small_context</code>.
+ * This structure is a context for Hamsi-224 computations. It is
+ * identical to the common <code>sph_hamsi_small_context</code>.
  */
-typedef sph_groestl_small_context sph_groestl224_context;
+typedef sph_hamsi_small_context sph_hamsi224_context;
 
 /**
- * This structure is a context for Groestl-256 computations. It is
- * identical to the common <code>sph_groestl_small_context</code>.
+ * This structure is a context for Hamsi-256 computations. It is
+ * identical to the common <code>sph_hamsi_small_context</code>.
  */
-typedef sph_groestl_small_context sph_groestl256_context;
+typedef sph_hamsi_small_context sph_hamsi256_context;
 
 /**
- * This structure is a context for Groestl-384 and Groestl-512 computations:
+ * This structure is a context for Hamsi-384 and Hamsi-512 computations:
  * it contains the intermediate values and some data from the last
- * entered block. Once a Groestl computation has been performed, the
+ * entered block. Once a Hamsi computation has been performed, the
  * context can be reused for another computation.
  *
- * The contents of this structure are private. A running Groestl
+ * The contents of this structure are private. A running Hamsi
  * computation can be cloned by copying the context (e.g. with a simple
  * <code>memcpy()</code>).
  */
 typedef struct {
 #ifndef DOXYGEN_IGNORE
-	unsigned char buf[128];    /* first field, for alignment */
-	size_t ptr;
-	union {
-#if SPH_64
-		sph_u64 wide[16];
-#endif
-		sph_u32 narrow[32];
-	} state;
+	unsigned char partial[8];
+	size_t partial_len;
+	sph_u32 h[16];
 #if SPH_64
 	sph_u64 count;
 #else
 	sph_u32 count_high, count_low;
 #endif
 #endif
-} sph_groestl_big_context;
+} sph_hamsi_big_context;
 
 /**
- * This structure is a context for Groestl-384 computations. It is
- * identical to the common <code>sph_groestl_small_context</code>.
+ * This structure is a context for Hamsi-384 computations. It is
+ * identical to the common <code>sph_hamsi_small_context</code>.
  */
-typedef sph_groestl_big_context sph_groestl384_context;
+typedef sph_hamsi_big_context sph_hamsi384_context;
 
 /**
- * This structure is a context for Groestl-512 computations. It is
- * identical to the common <code>sph_groestl_small_context</code>.
+ * This structure is a context for Hamsi-512 computations. It is
+ * identical to the common <code>sph_hamsi_small_context</code>.
  */
-typedef sph_groestl_big_context sph_groestl512_context;
+typedef sph_hamsi_big_context sph_hamsi512_context;
 
 /**
- * Initialize a Groestl-224 context. This process performs no memory allocation.
+ * Initialize a Hamsi-224 context. This process performs no memory allocation.
  *
- * @param cc   the Groestl-224 context (pointer to a
- *             <code>sph_groestl224_context</code>)
+ * @param cc   the Hamsi-224 context (pointer to a
+ *             <code>sph_hamsi224_context</code>)
  */
-void sph_groestl224_init(void *cc);
+void sph_hamsi224_init(void *cc);
 
 /**
  * Process some data bytes. It is acceptable that <code>len</code> is zero
  * (in which case this function does nothing).
  *
- * @param cc     the Groestl-224 context
+ * @param cc     the Hamsi-224 context
  * @param data   the input data
  * @param len    the input data length (in bytes)
  */
-void sph_groestl224(void *cc, const void *data, size_t len);
+void sph_hamsi224(void *cc, const void *data, size_t len);
 
 /**
- * Terminate the current Groestl-224 computation and output the result into
+ * Terminate the current Hamsi-224 computation and output the result into
  * the provided buffer. The destination buffer must be wide enough to
  * accomodate the result (28 bytes). The context is automatically
  * reinitialized.
  *
- * @param cc    the Groestl-224 context
+ * @param cc    the Hamsi-224 context
  * @param dst   the destination buffer
  */
-void sph_groestl224_close(void *cc, void *dst);
+void sph_hamsi224_close(void *cc, void *dst);
 
 /**
  * Add a few additional bits (0 to 7) to the current computation, then
@@ -179,42 +170,42 @@ void sph_groestl224_close(void *cc, void *dst);
  * numbered 7 downto 8-n (this is the big-endian convention at the byte
  * level). The context is automatically reinitialized.
  *
- * @param cc    the Groestl-224 context
+ * @param cc    the Hamsi-224 context
  * @param ub    the extra bits
  * @param n     the number of extra bits (0 to 7)
  * @param dst   the destination buffer
  */
-void sph_groestl224_addbits_and_close(
+void sph_hamsi224_addbits_and_close(
 	void *cc, unsigned ub, unsigned n, void *dst);
 
 /**
- * Initialize a Groestl-256 context. This process performs no memory allocation.
+ * Initialize a Hamsi-256 context. This process performs no memory allocation.
  *
- * @param cc   the Groestl-256 context (pointer to a
- *             <code>sph_groestl256_context</code>)
+ * @param cc   the Hamsi-256 context (pointer to a
+ *             <code>sph_hamsi256_context</code>)
  */
-void sph_groestl256_init(void *cc);
+void sph_hamsi256_init(void *cc);
 
 /**
  * Process some data bytes. It is acceptable that <code>len</code> is zero
  * (in which case this function does nothing).
  *
- * @param cc     the Groestl-256 context
+ * @param cc     the Hamsi-256 context
  * @param data   the input data
  * @param len    the input data length (in bytes)
  */
-void sph_groestl256(void *cc, const void *data, size_t len);
+void sph_hamsi256(void *cc, const void *data, size_t len);
 
 /**
- * Terminate the current Groestl-256 computation and output the result into
+ * Terminate the current Hamsi-256 computation and output the result into
  * the provided buffer. The destination buffer must be wide enough to
  * accomodate the result (32 bytes). The context is automatically
  * reinitialized.
  *
- * @param cc    the Groestl-256 context
+ * @param cc    the Hamsi-256 context
  * @param dst   the destination buffer
  */
-void sph_groestl256_close(void *cc, void *dst);
+void sph_hamsi256_close(void *cc, void *dst);
 
 /**
  * Add a few additional bits (0 to 7) to the current computation, then
@@ -224,42 +215,42 @@ void sph_groestl256_close(void *cc, void *dst);
  * numbered 7 downto 8-n (this is the big-endian convention at the byte
  * level). The context is automatically reinitialized.
  *
- * @param cc    the Groestl-256 context
+ * @param cc    the Hamsi-256 context
  * @param ub    the extra bits
  * @param n     the number of extra bits (0 to 7)
  * @param dst   the destination buffer
  */
-void sph_groestl256_addbits_and_close(
+void sph_hamsi256_addbits_and_close(
 	void *cc, unsigned ub, unsigned n, void *dst);
 
 /**
- * Initialize a Groestl-384 context. This process performs no memory allocation.
+ * Initialize a Hamsi-384 context. This process performs no memory allocation.
  *
- * @param cc   the Groestl-384 context (pointer to a
- *             <code>sph_groestl384_context</code>)
+ * @param cc   the Hamsi-384 context (pointer to a
+ *             <code>sph_hamsi384_context</code>)
  */
-void sph_groestl384_init(void *cc);
+void sph_hamsi384_init(void *cc);
 
 /**
  * Process some data bytes. It is acceptable that <code>len</code> is zero
  * (in which case this function does nothing).
  *
- * @param cc     the Groestl-384 context
+ * @param cc     the Hamsi-384 context
  * @param data   the input data
  * @param len    the input data length (in bytes)
  */
-void sph_groestl384(void *cc, const void *data, size_t len);
+void sph_hamsi384(void *cc, const void *data, size_t len);
 
 /**
- * Terminate the current Groestl-384 computation and output the result into
+ * Terminate the current Hamsi-384 computation and output the result into
  * the provided buffer. The destination buffer must be wide enough to
  * accomodate the result (48 bytes). The context is automatically
  * reinitialized.
  *
- * @param cc    the Groestl-384 context
+ * @param cc    the Hamsi-384 context
  * @param dst   the destination buffer
  */
-void sph_groestl384_close(void *cc, void *dst);
+void sph_hamsi384_close(void *cc, void *dst);
 
 /**
  * Add a few additional bits (0 to 7) to the current computation, then
@@ -269,42 +260,42 @@ void sph_groestl384_close(void *cc, void *dst);
  * numbered 7 downto 8-n (this is the big-endian convention at the byte
  * level). The context is automatically reinitialized.
  *
- * @param cc    the Groestl-384 context
+ * @param cc    the Hamsi-384 context
  * @param ub    the extra bits
  * @param n     the number of extra bits (0 to 7)
  * @param dst   the destination buffer
  */
-void sph_groestl384_addbits_and_close(
+void sph_hamsi384_addbits_and_close(
 	void *cc, unsigned ub, unsigned n, void *dst);
 
 /**
- * Initialize a Groestl-512 context. This process performs no memory allocation.
+ * Initialize a Hamsi-512 context. This process performs no memory allocation.
  *
- * @param cc   the Groestl-512 context (pointer to a
- *             <code>sph_groestl512_context</code>)
+ * @param cc   the Hamsi-512 context (pointer to a
+ *             <code>sph_hamsi512_context</code>)
  */
-void sph_groestl512_init(void *cc);
+void sph_hamsi512_init(void *cc);
 
 /**
  * Process some data bytes. It is acceptable that <code>len</code> is zero
  * (in which case this function does nothing).
  *
- * @param cc     the Groestl-512 context
+ * @param cc     the Hamsi-512 context
  * @param data   the input data
  * @param len    the input data length (in bytes)
  */
-void sph_groestl512(void *cc, const void *data, size_t len);
+void sph_hamsi512(void *cc, const void *data, size_t len);
 
 /**
- * Terminate the current Groestl-512 computation and output the result into
+ * Terminate the current Hamsi-512 computation and output the result into
  * the provided buffer. The destination buffer must be wide enough to
  * accomodate the result (64 bytes). The context is automatically
  * reinitialized.
  *
- * @param cc    the Groestl-512 context
+ * @param cc    the Hamsi-512 context
  * @param dst   the destination buffer
  */
-void sph_groestl512_close(void *cc, void *dst);
+void sph_hamsi512_close(void *cc, void *dst);
 
 /**
  * Add a few additional bits (0 to 7) to the current computation, then
@@ -314,13 +305,15 @@ void sph_groestl512_close(void *cc, void *dst);
  * numbered 7 downto 8-n (this is the big-endian convention at the byte
  * level). The context is automatically reinitialized.
  *
- * @param cc    the Groestl-512 context
+ * @param cc    the Hamsi-512 context
  * @param ub    the extra bits
  * @param n     the number of extra bits (0 to 7)
  * @param dst   the destination buffer
  */
-void sph_groestl512_addbits_and_close(
+void sph_hamsi512_addbits_and_close(
 	void *cc, unsigned ub, unsigned n, void *dst);
+
+
 
 #ifdef __cplusplus
 }
