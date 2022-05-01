6c0fdcf84dd4268d3af2d72179eec66b804b3968
98Forty <93034680+98Forty@users.noreply.github.com>
2022-05-01 07:45:37 +0000

    Stubs

---
+++ src/tor/config.h
@@ -0,0 +1,15 @@
+/* Copyright (c) 2001 Matej Pfajfar.
+ * Copyright (c) 2001-2004, Roger Dingledine.
+ * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
+ * Copyright (c) 2007-2013, The Tor Project, Inc. */
+/* See LICENSE for licensing information */
+
+/**
+ * \file config.h
+ * \brief Header file for config.c.
+ **/
+
+#ifndef TOR_CONFIG_H
+#define TOR_CONFIG_H
+
+const char *get_
