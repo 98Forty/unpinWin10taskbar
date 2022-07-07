12d71ab1ea13c7a8d588724fcff49dccab5fa5c9
98Forty <93034680+98Forty@users.noreply.github.com>
2022-07-07 16:08:01 +0000

    merge README

--- src/tor/relay.c
+++ src/tor/relay.c
@@ -23,4 +23,265 @@
 #include "connection.h"
 #include "connection_edge.h"
 #include "connection_or.h"
-#inc
+#include "control.h"
+#include "geoip.h"
+#include "onion_main.h"
+#include "mempool.h"
+#include "networkstatus.h"
+#include "nodelist.h"
+#include "onion.h"
+#include "policies.h"
+#include "reasons.h"
+#include "relay.h"
+#include "rendcommon.h"
+#include "router.h"
+#include "routerlist.h"
+#include "routerparse.h"
+
+static edge_connection_t *relay_lookup_conn(circuit_t *circ, cell_t *cell,
+                                            cell_direction_t cell_direction,
+                                            crypt_path_t *layer_hint);
+
+static int connection_edge_process_relay_cell(cell_t *cell, circuit_t *circ,
+                                              edge_connection_t *conn,
+                                              crypt_path_t *layer_hint);
+static void circuit_consider_sending_sendme(circuit_t *circ,
+                                            crypt_path_t *layer_hint);
+static void circuit_resume_edge_reading(circuit_t *circ,
+                                        crypt_path_t *layer_hint);
+static int circuit_resume_edge_reading_helper(edge_connection_t *conn,
+                                              circuit_t *circ,
+                                              crypt_path_t *layer_hint);
+static int circuit_consider_stop_edge_reading(circuit_t *circ,
+                                              crypt_path_t *layer_hint);
+static int circuit_queue_streams_are_blocked(circuit_t *circ);
+static void adjust_exit_policy_from_exitpolicy_failure(origin_circuit_t *circ,
+                                                  entry_connection_t *conn,
+                                                  node_t *node,
+                                                  const tor_addr_t *addr);
+#if 0
+static int get_max_middle_cells(void);
+#endif
+
+/** Stop reading on edge connections when we have this many cells
+ * waiting on the appropriate queue. */
+#define CELL_QUEUE_HIGHWATER_SIZE 256
+/** Start reading from edge connections again when we get down to this many
+ * cells. */
+#define CELL_QUEUE_LOWWATER_SIZE 64
+
+/** Stats: how many relay cells have originated at this hop, or have
+ * been relayed onward (not recognized at this hop)?
+ */
+uint64_t stats_n_relay_cells_relayed = 0;
+/** Stats: how many relay cells have been delivered to streams at this
+ * hop?
+ */
+uint64_t stats_n_relay_cells_delivered = 0;
+
+/** Used to tell which stream to read from first on a circuit. */
+static tor_weak_rng_t stream_choice_rng = TOR_WEAK_RNG_INIT;
+
+/** Update digest from the payload of cell. Assign integrity part to
+ * cell.
+ */
+static void
+relay_set_digest(crypto_digest_t *digest, cell_t *cell)
+{
+  char integrity[4];
+  relay_header_t rh;
+
+  crypto_digest_add_bytes(digest, (char*)cell->payload, CELL_PAYLOAD_SIZE);
+  crypto_digest_get_digest(digest, integrity, 4);
+//  log_fn(LOG_DEBUG,"Putting digest of %u %u %u %u into relay cell.",
+//    integrity[0], integrity[1], integrity[2], integrity[3]);
+  relay_header_unpack(&rh, cell->payload);
+  memcpy(rh.integrity, integrity, 4);
+  relay_header_pack(cell->payload, &rh);
+}
+
+/** Does the digest for this circuit indicate that this cell is for us?
+ *
+ * Update digest from the payload of cell (with the integrity part set
+ * to 0). If the integrity part is valid, return 1, else restore digest
+ * and cell to their original state and return 0.
+ */
+static int
+relay_digest_matches(crypto_digest_t *digest, cell_t *cell)
+{
+  char received_integrity[4], calculated_integrity[4];
+  relay_header_t rh;
+  crypto_digest_t *backup_digest=NULL;
+
+  backup_digest = crypto_digest_dup(digest);
+
+  relay_header_unpack(&rh, cell->payload);
+  memcpy(received_integrity, rh.integrity, 4);
+  memset(rh.integrity, 0, 4);
+  relay_header_pack(cell->payload, &rh);
+
+//  log_fn(LOG_DEBUG,"Reading digest of %u %u %u %u from relay cell.",
+//    received_integrity[0], received_integrity[1],
+//    received_integrity[2], received_integrity[3]);
+
+  crypto_digest_add_bytes(digest, (char*) cell->payload, CELL_PAYLOAD_SIZE);
+  crypto_digest_get_digest(digest, calculated_integrity, 4);
+
+  if (tor_memneq(received_integrity, calculated_integrity, 4)) {
+//    log_fn(LOG_INFO,"Recognized=0 but bad digest. Not recognizing.");
+// (%d vs %d).", received_integrity, calculated_integrity);
+    /* restore digest to its old form */
+    crypto_digest_assign(digest, backup_digest);
+    /* restore the relay header */
+    memcpy(rh.integrity, received_integrity, 4);
+    relay_header_pack(cell->payload, &rh);
+    crypto_digest_free(backup_digest);
+    return 0;
+  }
+  crypto_digest_free(backup_digest);
+  return 1;
+}
+
+/** Apply <b>cipher</b> to CELL_PAYLOAD_SIZE bytes of <b>in</b>
+ * (in place).
+ *
+ * If <b>encrypt_mode</b> is 1 then encrypt, else decrypt.
+ *
+ * Return -1 if the crypto fails, else return 0.
+ */
+static int
+relay_crypt_one_payload(crypto_cipher_t *cipher, uint8_t *in,
+                        int encrypt_mode)
+{
+  int r;
+  (void)encrypt_mode;
+  r = crypto_cipher_crypt_inplace(cipher, (char*) in, CELL_PAYLOAD_SIZE);
+
+  if (r) {
+    log_warn(LD_BUG,"Error during relay encryption");
+    return -1;
+  }
+  return 0;
+}
+
+/** Receive a relay cell:
+ *  - Crypt it (encrypt if headed toward the origin or if we <b>are</b> the
+ *    origin; decrypt if we're headed toward the exit).
+ *  - Check if recognized (if exitward).
+ *  - If recognized and the digest checks out, then find if there's a stream
+ *    that the cell is intended for, and deliver it to the right
+ *    connection_edge.
+ *  - If not recognized, then we need to relay it: append it to the appropriate
+ *    cell_queue on <b>circ</b>.
+ *
+ * Return -<b>reason</b> on failure.
+ */
+int
+circuit_receive_relay_cell(cell_t *cell, circuit_t *circ,
+                           cell_direction_t cell_direction)
+{
+  channel_t *chan = NULL;
+  crypt_path_t *layer_hint=NULL;
+  char recognized=0;
+  int reason;
+
+  tor_assert(cell);
+  tor_assert(circ);
+  tor_assert(cell_direction == CELL_DIRECTION_OUT ||
+             cell_direction == CELL_DIRECTION_IN);
+  if (circ->marked_for_close)
+    return 0;
+
+  if (relay_crypt(circ, cell, cell_direction, &layer_hint, &recognized) < 0) {
+    log_warn(LD_BUG,"relay crypt failed. Dropping connection.");
+    return -END_CIRC_REASON_INTERNAL;
+  }
+
+  if (recognized) {
+    edge_connection_t *conn = NULL;
+
+    if (circ->purpose == CIRCUIT_PURPOSE_PATH_BIAS_TESTING) {
+      pathbias_check_probe_response(circ, cell);
+
+      /* We need to drop this cell no matter what to avoid code that expects
+       * a certain purpose (such as the hidserv code). */
+      return 0;
+    }
+
+    conn = relay_lookup_conn(circ, cell, cell_direction,
+                                                layer_hint);
+    if (cell_direction == CELL_DIRECTION_OUT) {
+      ++stats_n_relay_cells_delivered;
+      log_debug(LD_OR,"Sending away from origin.");
+      if ((reason=connection_edge_process_relay_cell(cell, circ, conn, NULL))
+          < 0) {
+        log_fn(LOG_PROTOCOL_WARN, LD_PROTOCOL,
+               "connection_edge_process_relay_cell (away from origin) "
+               "failed.");
+        return reason;
+      }
+    }
+    if (cell_direction == CELL_DIRECTION_IN) {
+      ++stats_n_relay_cells_delivered;
+      log_debug(LD_OR,"Sending to origin.");
+      if ((reason = connection_edge_process_relay_cell(cell, circ, conn,
+                                                       layer_hint)) < 0) {
+        log_warn(LD_OR,
+                 "connection_edge_process_relay_cell (at origin) failed.");
+        return reason;
+      }
+    }
+    return 0;
+  }
+
+  /* not recognized. pass it on. */
+  if (cell_direction == CELL_DIRECTION_OUT) {
+    cell->circ_id = circ->n_circ_id; /* switch it */
+    chan = circ->n_chan;
+  } else if (! CIRCUIT_IS_ORIGIN(circ)) {
+    cell->circ_id = TO_OR_CIRCUIT(circ)->p_circ_id; /* switch it */
+    chan = TO_OR_CIRCUIT(circ)->p_chan;
+  } else {
+    log_fn(LOG_PROTOCOL_WARN, LD_OR,
+           "Dropping unrecognized inbound cell on origin circuit.");
+    /* If we see unrecognized cells on path bias testing circs,
+     * it's bad mojo. Those circuits need to die.
+     * XXX: Shouldn't they always die? */
+    if (circ->purpose == CIRCUIT_PURPOSE_PATH_BIAS_TESTING) {
+      TO_ORIGIN_CIRCUIT(circ)->path_state = PATH_STATE_USE_FAILED;
+      return -END_CIRC_REASON_TORPROTOCOL;
+    } else {
+      return 0;
+    }
+  }
+
+  if (!chan) {
+    // XXXX Can this splice stuff be done more cleanly?
+    if (! CIRCUIT_IS_ORIGIN(circ) &&
+        TO_OR_CIRCUIT(circ)->rend_splice &&
+        cell_direction == CELL_DIRECTION_OUT) {
+      or_circuit_t *splice = TO_OR_CIRCUIT(circ)->rend_splice;
+      tor_assert(circ->purpose == CIRCUIT_PURPOSE_REND_ESTABLISHED);
+      tor_assert(splice->base_.purpose == CIRCUIT_PURPOSE_REND_ESTABLISHED);
+      cell->circ_id = splice->p_circ_id;
+      cell->command = CELL_RELAY; /* can't be relay_early anyway */
+      if ((reason = circuit_receive_relay_cell(cell, TO_CIRCUIT(splice),
+                                               CELL_DIRECTION_IN)) < 0) {
+        log_warn(LD_REND, "Error relaying cell across rendezvous; closing "
+                 "circuits");
+        /* XXXX Do this here, or just return -1? */
+        circuit_mark_for_close(circ, -reason);
+        return reason;
+      }
+      return 0;
+    }
+    log_fn(LOG_PROTOCOL_WARN, LD_PROTOCOL,
+           "Didn't recognize cell, but circ stops here! Closing circ.");
+    return -END_CIRC_REASON_TORPROTOCOL;
+  }
+
+  log_debug(LD_OR,"Passing on unrecognized cell.");
+
+  ++stats_n_relay_cells_relayed; /* XXXX no longer quite accurate {cells}
+                                  * we might kill the circ before we relay
+                                  * the
